c9ffd3f4557e4bd9f49f7803e7108aba836f1daf
Himanshu Khudania <hkhudania@gmail.com>
2019-09-25 23:36:14 +0530

    Resetting and completing the First App

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQues(int score) {
     _totalScore += score;
 
@@ -76,7 +83,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
